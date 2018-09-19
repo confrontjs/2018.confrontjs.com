5e55764e6fa6063878a1bd792fe9d0a9cfff37ee
Piotr Kowalski <piecioshka@gmail.com>
2018-09-19 17:30:02 +0200

    Support mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
 (function () {
 
     const $navbar = document.querySelector('.navbar');
+    const $navbarToggler = document.querySelector('.navbar-toggler');
+    const $navbarMenu = document.querySelector('.navbar-nav').parentElement;
 
     function updateCompactClass() {
         const isScrolled = (window.scrollY > 0);
+        const isMobile = $navbarMenu.classList.contains('show');
+
+        if (isMobile) {
+            return;
+        }
 
         if (isScrolled) {
             $navbar.classList.add('navbar-compact');
@@ -14,5 +21,9 @@
 
     updateCompactClass();
     window.addEventListener('scroll', updateCompactClass);
+
+    $navbarToggler.addEventListener('click', () => {
+        $navbarMenu.classList.toggle('show');
+    });
 
 })();
