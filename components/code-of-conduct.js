5987447dfdeb12a0eb385ee0763515adb9eed641
Piotr Kowalski <piecioshka@gmail.com>
2018-10-16 17:37:01 +0200

    Update CoC

--- components/code-of-conduct.js
+++ components/code-of-conduct.js
@@ -9,13 +9,21 @@ class CodeOfConductComponent extends Component {
                 </h2>
 
                 <p>
-                    We are aware of diversity issues in our industry and we tackle
-                    them in a gentle way.<br/>We use Berlin Code of Conduct and we base
-                    our cooperation on trust and mutual respect for everyone.
+                    If you are subject to or witness unacceptable behavior,
+                    or have any other concerns, please notify a conference
+                    organizer as soon as possible. The conference organizers
+                    are: Katarzyna Grabowska, Piotr Kowalski and Piotr Zientara.
+                    You can contact them directly or via warsawjs.slack.com
+                    Additionally, community organizers are available to help
+                    community members engage with local law enforcement or to
+                    otherwise help those experiencing unacceptable behavior feel
+                    safe. In the context of in-person events, organizers will
+                    also provide escorts as desired by the person experiencing
+                    distress.
                 </p>
 
                 <p>
-                    Please visit&nbsp;
+                    Please read the full Code of Conduct:&nbsp;
                     <a
                             href="http://berlincodeofconduct.org"
                             target="_blank"
